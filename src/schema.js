5fa7bcccc39d9111b24d0b3b91c81e580d5abeb3
Luca Di Fazio <highliuk@gmail.com>
2021-11-09 20:40:55 +0100

    Fix relation directions

--- src/schema.js
+++ src/schema.js
@@ -3,8 +3,8 @@ const { gql } = require("apollo-server")
 const schema = gql`
   type Person {
     name: String!
-    father: Person @relationship(type: "CHILD_OF", direction: IN)
-    children: [Person!]! @relationship(type: "CHILD_OF", direction: OUT)
+    father: Person @relationship(type: "CHILD_OF", direction: OUT)
+    children: [Person!]! @relationship(type: "CHILD_OF", direction: IN)
   }
 `
 
